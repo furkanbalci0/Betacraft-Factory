d723b6a4b2e10354651ec7f43b3d3e032d82a81b
Furkan BALCI <blcfrkn@hotmail.com>
2021-08-14 12:57:05 +0300

    Test

--- src/main/java/org/betacraft/factory/customitem/CustomItem.java
+++ src/main/java/org/betacraft/factory/customitem/CustomItem.java
@@ -1,6 +1,176 @@
 package org.betacraft.factory.customitem;
 
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.Arrays;
+
 public class CustomItem {
 
+    //VARIABLES
+    private ItemStack itemStack;
+    private String name;
+    private String lore;
 
+
+    //CONSTRUCTORS
+    public CustomItem(ItemStack itemStack) {
+        this(itemStack, null, null);
+    }
+
+    public CustomItem(Material material) {
+        this(new ItemStack(material), null, null);
+    }
+
+    public CustomItem(Material material, String name, String lore) {
+        this.itemStack = new ItemStack(material);
+        this.name = name;
+        this.lore = lore;
+    }
+
+    public CustomItem(ItemStack itemStack, String name, String lore) {
+        this.itemStack = itemStack;
+        this.name = name;
+        this.lore = lore;
+        this.updateItem();
+    }
+
+    //GETTER FUNCTIONS
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    //SETTER FUNCTIONS
+    public CustomItem setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+        this.updateItem();
+        return this;
+    }
+
+    public CustomItem setName(String name) {
+        this.name = name;
+        this.updateItem();
+        return this;
+    }
+
+    public CustomItem setLore(String lore) {
+        this.lore = lore;
+        this.updateItem();
+        return this;
+    }
+
+    //OTHER FUNCTIONS
+    public CustomItem setHideAttributes(boolean hide) {
+
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+
+        itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+
+        this.itemStack.setItemMeta(itemMeta);
+
+        return this;
+
+    }
+
+    public CustomItem setHideEnchants(boolean hide) {
+
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+
+        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+
+        this.itemStack.setItemMeta(itemMeta);
+
+        return this;
+
+    }
+
+    public CustomItem setHideUnbreakable(boolean hide) {
+
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+
+        itemMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+
+        this.itemStack.setItemMeta(itemMeta);
+
+        return this;
+
+    }
+
+
+    public CustomItem setGlow(boolean glow) {
+
+        this.itemStack.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 1);
+
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+
+        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_ATTRIBUTES);
+
+        this.itemStack.setItemMeta(itemMeta);
+
+        return this;
+
+    }
+
+    //STATIC FUNCTIONS
+    public static ItemStack create(final ItemStack itemStack, String name, String lore){
+
+        ItemStack item = itemStack.clone();
+
+        final ItemMeta meta = item.getItemMeta();
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        if (name != null) {
+            meta.setDisplayName(name);
+        }
+        if (lore != null) {
+            meta.setLore(Arrays.asList(lore.split("\\|\\|")));
+        }
+        item.setItemMeta(meta);
+        return item;
+
+    }
+    public static ItemStack create(final Material material, String name, String lore){
+        return create(new ItemStack(material), name, lore);
+    }
+
+    public static ItemStack createSkull(String playerName) {
+
+        ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+
+        SkullMeta meta = (SkullMeta) skull.getItemMeta();
+
+        meta.setOwner(playerName);
+
+        skull.setItemMeta(meta);
+
+        return skull;
+
+    }
+
+    private void updateItem() {
+
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+
+        if (name != null) {
+            itemMeta.setDisplayName(name);
+        }
+
+        if (lore != null) {
+            itemMeta.setLore(Arrays.asList(lore.split("\\|\\|")));
+        }
+
+        this.itemStack.setItemMeta(itemMeta);
+    }
 }
---
+++ src/main/java/org/betacraft/factory/utils/text/Text.java
@@ -0,0 +1,103 @@
+package org.betacraft.factory.utils.text;
+
+public class Text {
+
+
+    /**
+     * It parses long numbers.
+     * @param value It's a long numbers.
+     * @return Decomposes string.
+     */
+    public static String decompose(Object value) {
+        return value != null ? value.toString().replaceAll("(?<=\\d)(?=(\\d\\d\\d)+(?!\\d))", ",") : "0";
+    }
+
+
+    /**
+     *
+     * @param character Progress bar's character.
+     * @param percent From 0 to 100.
+     * @param width Determines how wide the progress bar will be.
+     * @return Sorts the specified characters.
+     */
+    public static String progress(char character, double percent, int width) {
+
+        int loop = (int) Math.floor(percent / 2.5);
+        StringBuilder list = new StringBuilder();
+
+        for (int i = 0; i < loop && i < width; i++) {
+            list.append(character);
+        }
+
+        for (int i = 0; i < (width - loop); i++) {
+            list.append("§7").append(character);
+        }
+        return list.toString();
+    }
+
+    /**
+     *
+     * @param character Progress bar's character.
+     * @param percent From 0 to 100.
+     * Width default by 40.
+     * @return Sorts the specified characters.
+     */
+    public static String progress(char character, double percent) {
+        return progress(character, percent, 40);
+    }
+
+    /**
+     *
+     * Character default by '?'
+     * @param percent From 0 to 100.
+     * Width default by 40.
+     * @return Sorts the specified characters.
+     */
+    public static String progress(double percent) {
+        return progress('?', percent, 40);
+    }
+
+
+    /**
+     *
+     * @param text The text you want to convert.
+     * @return Text you want to convereted but uncolored.
+     */
+    public static String uncolored(String text) {
+        return text.replace("§", "&")
+                .replace("&0", "")
+                .replace("&1", "")
+                .replace("&2", "")
+                .replace("&3", "")
+                .replace("&4", "")
+                .replace("&5", "")
+                .replace("&6", "")
+                .replace("&7", "")
+                .replace("&8", "")
+                .replace("&9", "")
+                .replace("&a", "")
+                .replace("&b", "")
+                .replace("&c", "")
+                .replace("&d", "")
+                .replace("&e", "")
+                .replace("&l", "")
+                .replace("&k", "")
+                .replace("&n", "")
+                .replace("&m", "")
+                .replace("&o", "")
+                .replace("&f", "")
+                .replace("&A", "")
+                .replace("&B", "")
+                .replace("&C", "")
+                .replace("&D", "")
+                .replace("&E", "")
+                .replace("&F", "")
+                .replace("&L", "")
+                .replace("&N", "")
+                .replace("&M", "")
+                .replace("&K", "")
+                .replace("&O", "");
+    }
+
+
+}
