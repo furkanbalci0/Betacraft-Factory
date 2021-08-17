586b4d5ccf69dea6eb67b7530d57edb680199781
Furkan BALCI <blcfrkn@hotmail.com>
2021-08-17 21:56:02 +0300

    new version

--- src/main/java/org/betacraft/factory/scoreboard/BScoreboard.java
+++ src/main/java/org/betacraft/factory/scoreboard/BScoreboard.java
@@ -5,54 +5,112 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class BScoreboard {
 
 
+    //VARIABLES
     private final Player player;
-    private Objective objective;
-    private Scoreboard board;
+    private final Objective objective;
+    private final Scoreboard board;
+    private final Map<Integer, String> lines;
 
+
+    /**
+     *
+     * @param player Whose scoreboard will be given.
+     */
     public BScoreboard(Player player) {
+
+        this.lines = new HashMap<>();
 
         this.player = player;
 
         this.board = Bukkit.getScoreboardManager().getNewScoreboard();
 
-        this.objective = board.registerNewObjective("", player.getName());
+        this.objective = board.registerNewObjective("124124", "dummy");
 
         this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
+        player.getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
+
         player.setScoreboard(board);
 
     }
 
-    public void setScoreBoard(Player player) {
+    /**
+     * Removing player's scoreboard.
+     */
+    public void removeScoreboard(){
+        this.player.getScoreboard().clearSlot(DisplaySlot.SIDEBAR);
+    }
 
-        Score onlineName = this.objective.getScore(ChatColor.GRAY + "� Online");
-        onlineName.setScore(15);
-        Team onlineCounter = board.registerNewTeam("onlineCounter");
+    /**
+     *
+     * @param line Line should be just from 0 to 15.
+     * @param text Each line must be different from each other. If you are going to write the same line, you should add a different color code to the beginning.
+     */
+    public void setLine(int line, String text) {
 
-        onlineCounter.addEntry(ChatColor.BLACK + "" + ChatColor.WHITE);
-
-        if (Bukkit.getOnlinePlayers().size() == 0) {
-            onlineCounter.setPrefix(ChatColor.DARK_RED + "0" + ChatColor.RED + "/" + ChatColor.DARK_RED + Bukkit.getMaxPlayers());
-        } else {
-            onlineCounter.setPrefix("" + ChatColor.DARK_RED + Bukkit.getOnlinePlayers().size() + ChatColor.RED + "/" + ChatColor.DARK_RED + Bukkit.getMaxPlayers());
+        if(line < 0 || line > 15){
+            throw new NumberFormatException("Line should be just from 0 to 15.");
         }
 
-        this.objective.getScore(ChatColor.BLACK + "" + ChatColor.WHITE).setScore(14);
+        String oldLine = lines.get(line);
 
-        Score money = this.objective.getScore(ChatColor.GRAY + "� Money");
-        money.setScore(13);
+        text = ChatColor.translateAlternateColorCodes('&', text.replace('§', '&'));
 
-        Team moneyCounter = board.registerNewTeam("moneyCounter");
-        moneyCounter.addEntry(ChatColor.RED + "" + ChatColor.WHITE);
-        moneyCounter.setPrefix(ChatColor.GREEN + "$" + 100);
-        this.objective.getScore(ChatColor.RED + "" + ChatColor.WHITE).setScore(12);
+        lines.put(line, text);
 
+        if (oldLine != null) {
 
-        player.setScoreboard(board);
+            board.resetScores(oldLine);
+
+        }
+
+        Team t = board.getTeam(line + "");
+
+        if (t == null) {
+            t = board.registerNewTeam(line + "");
+
+        }
+
+        t.setPrefix(text);
+
+        Score score = this.objective.getScore(text);
+
+        score.setScore(line);
+
+        Random random = new Random();
+
+        int colorLength = ChatColor.values().length;
+
+        t.addEntry(Arrays.stream(ChatColor.values()).toArray()[random.nextInt(colorLength)] + "" + Arrays.stream(ChatColor.values()).toArray()[random.nextInt(colorLength)]);
+
+        this.player.setScoreboard(this.board);
 
     }
+
+    /**
+     *
+     * @param line The line you want to delete from the scoreboard.
+     */
+    public void removeLine(int line){
+
+        String oldLine = lines.get(line);
+
+        if(oldLine == null) return;
+
+        lines.remove(line);
+
+        this.board.resetScores(oldLine);
+
+    }
+
+
 
 }
